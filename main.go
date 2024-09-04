0119a441f4a88eef89edc526921ed7b37a3db5f1
Gotkowski <paprykojad@gmail.com>
2024-09-04 11:08:55 +0200

    cały czas nie działa dodawanie 1 do liczby hitów bo moja metoda nie działa a ta z boot.dev się pierdoli

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
+type configApi struct {
+    fileserverHits int
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *configApi) middlewareMetricsInc(next *http.Handler) http.Handler {
+    cfg.fileserverHits++
+    return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *configApi) hitCount(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
 
 func main() {
     filepathRoot := "."
@@ -14,8 +37,14 @@ func main() {
         Addr: ":" + port,
     }
 
-    sm.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+    apiCfg := configApi {
+        fileserverHits: 0,
+    }
+
+    sm.Handle("/app/", apiCfg.middlewareMetricsInc(&http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
     sm.Handle("/assets/logo.png", http.FileServer(http.Dir(filepathRoot)))
     sm.Handle("/pikachu.png", http.FileServer(http.Dir("./assets/")))
+    sm.HandleFunc("/healthz", handlerReadiness)
+    sm.HandleFunc("/metrics", apiCfg.hitCount)
     srv.ListenAndServe()
 }
